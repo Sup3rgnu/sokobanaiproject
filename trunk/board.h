abc2da78911005598daf7f25164716c97281e572
Sup3rgnu <Sup3rgnu@18970548-fefa-58d0-73d4-541a78ed6038>
2010-09-21 13:28:15 +0000

    added board.*

---
+++ trunk/board.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   board.h
+ * Author: kungen
+ *
+ * Created on September 21, 2010, 3:00 PM
+ */
+
+#ifndef BOARD_H
+#define	BOARD_H
+
+class board{
+
+public:
+
+    board(std::string board);
+
+private:
+    
+    int size; 
+
+
+};
+
+
+#endif	/* BOARD_H */
+
