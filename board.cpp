4a448060972cbaaf79fd93011c1cf149baac7e47
pjagmalm <pjagmalm@18970548-fefa-58d0-73d4-541a78ed6038>
2010-09-21 15:37:20 +0000

    local_client + row count

--- board.cpp
+++ board.cpp
@@ -1,12 +1,23 @@
 
 #include <iostream>
+#include <sstream>
+#include <vector>
 #include "board.h"
 
-board::board (std::string board){
-    
- int cols = board.find_first_of('\n');
- std::cout << "cols = " + cols;
- //hej
+
+board::board (std::string board1){
+
+ std::vector< std::string > result;
+ int cols, rows;
+
+ std::stringstream ss(board1);
+ std::string item;
+
+ while(std::getline(ss, item, '\n')) {
+    result.push_back(item);
+ }
+
+ std::cout << "rows = " << result.size() << '\n';
 
 
 }
---
+++ local_client.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include "board.h"
+
+
+main() {
+
+ std::string s1 = "########\n#   # .#\n#   $$.#\n####   #\n   #@ ##\n   ####";
+ board b1(s1);
+
+}
