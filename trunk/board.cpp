e10c979e4bb1ced780d34e8a0eb33f36d9bb39cf
robzet <robzet@18970548-fefa-58d0-73d4-541a78ed6038>
2010-09-21 16:16:16 +0000

    cols added.

--- trunk/board.cpp
+++ trunk/board.cpp
@@ -6,17 +6,22 @@
 
 board::board (std::string board1){
 
- std::vector< std::string > result;
- int cols, rows;
-
+std::vector< std::string > result;
+int cols = 0;
+int rows = 0;
+int current = 0;
  std::stringstream ss(board1);
  std::string item;
 
  while(std::getline(ss, item, '\n')) {
+	current = item.size();
+	if(current > cols) {
+		cols = current;
+	}
     result.push_back(item);
  }
 
- std::cout << "rows = " << result.size() << '\n';
+ std::cout << "rows = " << result.size() << " cols = " << cols << '\n';
 
 
 }
--- trunk/makefile
+++ trunk/makefile
@@ -3,5 +3,5 @@ all:	client	local_client
 client:
 		g++ client.cc board.cpp -lboost_system -o client
 		
-local_client:
+local_client: 
 		g++ local_client.cpp board.cpp -lboost_system -o local_client
