97de667befac735cdd94711dd35afe92ff7c3d60
pjagmalm <pjagmalm@18970548-fefa-58d0-73d4-541a78ed6038>
2010-09-26 19:42:35 +0000

    boards

--- board.h
+++ board.h
@@ -33,7 +33,7 @@ private:
     pair<char, bool> move_result;			// Varje move genererar en riktning U/D/L/R samt talar om ifall en l�da p�verkats.
     vector< pair <char, bool> > solution;	// Vektorn h�ller samtliga aktuella move_result pairs.
     vector< vector<char> > theboard;		// Arren blev en vektor.
-    vector <board> visited_boards;
+    vector< vector< vector<char> > > visited_boards;
 };
 
 #endif	/* BOARD_H */
