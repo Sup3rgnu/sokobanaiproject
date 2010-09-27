9af616b05f277c7e52dbff476c996f1c52581980
robzet <robzet@18970548-fefa-58d0-73d4-541a78ed6038>
2010-09-27 15:29:52 +0000

    Rättade +3 buggen i validateMove().

--- board.cpp
+++ board.cpp
@@ -146,7 +146,7 @@ bool board::validateMove(char c) {
 
 				ok = false;
 			}
-			if(theboard[(ppos.first)+3][ppos.second] == '#' && (theboard[(ppos.first)+2][(ppos.second)-1] == '#' || theboard[(ppos.first)+2][(ppos.second)+1] == '#')){
+			else if(theboard[(ppos.first)+3][ppos.second] == '#' && (theboard[(ppos.first)+2][(ppos.second)-1] == '#' || theboard[(ppos.first)+2][(ppos.second)+1] == '#')){
 				if(DEBUG_VALIDATEMOVE == 1 && true) { DEBUG(cout << "This move would push the box into a corner! Not OK." << endl); }
 
 				ok = false;
@@ -170,7 +170,7 @@ bool board::validateMove(char c) {
 
 				ok = false;
 			}
-			if(theboard[(ppos.first)][ppos.second-3] == '#' && (theboard[(ppos.first)-1][(ppos.second)-2] == '#' || theboard[(ppos.first)+1][(ppos.second)-2] == '#')){
+			else if(theboard[(ppos.first)][ppos.second-3] == '#' && (theboard[(ppos.first)-1][(ppos.second)-2] == '#' || theboard[(ppos.first)+1][(ppos.second)-2] == '#')){
 				if(DEBUG_VALIDATEMOVE == 1 && true) { DEBUG(cout << "This move would push the box into a corner! Not OK." << endl); }
 
 				ok = false;
@@ -194,7 +194,7 @@ bool board::validateMove(char c) {
 
 				ok = false;
 			}
-			if(theboard[ppos.first][ppos.second+3] == '#' && (theboard[(ppos.first)-1][ppos.second+2] == '#' || theboard[ppos.first+1][ppos.second+2] == '#')){
+			else if(theboard[ppos.first][ppos.second+3] == '#' && (theboard[(ppos.first)-1][ppos.second+2] == '#' || theboard[ppos.first+1][ppos.second+2] == '#')){
 				if(DEBUG_VALIDATEMOVE == 1 && true) { DEBUG(cout << "This move would push the box into a corner! Not OK." << endl); }
 
 				ok = false;
