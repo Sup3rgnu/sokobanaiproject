773f87fee3e88945ce345c13fb9833a3f9fd2d29
pjagmalm <pjagmalm@18970548-fefa-58d0-73d4-541a78ed6038>
2010-10-16 20:39:39 +0000

    stick to box

--- trunk/board.h
+++ trunk/board.h
@@ -35,6 +35,7 @@ public:
     void prepareBoard();
     pair<int, int> goalDistance();
     int printhash();
+    bool notLeavingBox(char);
 private:
 
     char currentMove;						// Det move vi just nu beaktar. Skall vara VERSAL.
