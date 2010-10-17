a400db1a75c29628d227185a815e644d066fecb2
robzet <robzet@18970548-fefa-58d0-73d4-541a78ed6038>
2010-10-17 18:07:05 +0000

    Bugfix HASHTABLE + REACH combo. Boards 1,131,132,133,135 OK.

--- trunk/board.h
+++ trunk/board.h
@@ -12,8 +12,8 @@
 #define USE_HASHTABLE	1
 #define USE_TR1_HASH	0
 #define USE_WALL		0
-#define USE_REACH		0
-#define USE_STICK		0
+#define USE_REACH		1
+#define USE_STICK		1
 
 #include <string>
 #include <vector>
